aaeb7cde2618c28d512d2c625858e296f289ccd2
Stefanny Soares <stefannysoares1103@gmail.com>
2023-02-28 08:20:31 -0300

    commented codes.

--- controller/Exrecursiva02C.java
+++ controller/Exrecursiva02C.java
@@ -13,14 +13,15 @@ public class Exrecursiva02C {
    // System.out.println(s.length());
 
     public int nInt (int n, int m){
-        if (n<m){
+
+        if (n<m){// ponto de parada
             m = m/10;
             
             String s = Integer.toString(m);
             int dig = s.length(); 
             return dig;
         }
-        if (n / m >= 1){
+        if (n / m >= 1){ // condição para a chamada da próxima recursiva
             return nInt(n, m*10);
 
         }
